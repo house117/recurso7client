0c17a52ba561de9e73197ae1b24b9d3fa0b24591
house117 <josefloresgarcia17@gmail.com>
2019-11-15 16:45:31 -0600

    diezmos y ofrendas crud finished

--- src/app/pages/home-results/home-results.page.ts
+++ src/app/pages/home-results/home-results.page.ts
@@ -1,13 +1,13 @@
-import { ActionSheetController } from '@ionic/angular';
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { Observable } from 'rxjs';
-import { IonList } from '@ionic/angular';
+import { ActionSheetController } from "@ionic/angular";
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { Observable } from "rxjs";
+import { IonList } from "@ionic/angular";
 import { NgModel } from "@angular/forms";
 import { ActivatedRoute, NavigationExtras } from "@angular/router";
 import { HttpParams } from "@angular/common/http";
 import { Router } from "@angular/router";
-import { IonInfiniteScroll } from '@ionic/angular';
+import { IonInfiniteScroll } from "@ionic/angular";
 import {
     NavController,
     AlertController,
@@ -22,206 +22,271 @@ import {
     templateUrl: "./home-results.page.html",
     styleUrls: ["./home-results.page.scss"]
 })
-export class HomeResultsPage implements OnInit{
-   
+export class HomeResultsPage implements OnInit {
+    @ViewChild("lista") lista: IonList;
+    conteo: any;
+    usuarios: any;
+    url = "http://localhost:5000/api/conteo/";
 
-  @ViewChild('lista') lista: IonList;
-  conteo: any;
-  url = "http://localhost:5000/api/conteo/";
-
-  constructor(
-    private actionSheetCtrl: ActionSheetController,
-    private menuCtrl: MenuController, 
-    private toastCtrl: ToastController,
-    private http: HttpClient,
-    public navCtrl: NavController,
-    public popoverCtrl: PopoverController,
-    public alertCtrl: AlertController,
-    public modalCtrl: ModalController,
-    private activatedRoute: ActivatedRoute,
-    private router: Router 
+    constructor(
+        private actionSheetCtrl: ActionSheetController,
+        private menuCtrl: MenuController,
+        private toastCtrl: ToastController,
+        private http: HttpClient,
+        public navCtrl: NavController,
+        public popoverCtrl: PopoverController,
+        public alertCtrl: AlertController,
+        public modalCtrl: ModalController,
+        private activatedRoute: ActivatedRoute,
+        private router: Router
     ) {}
 
-
-  ngOnInit() {
-    //this.usuarios = this.getUsers();
-    this.http.get(`http://localhost:5000/api/conteo/`).subscribe(res => {
+    ngOnInit() {
+        //this.usuarios = this.getUsers();
+        this.http.get(`http://localhost:5000/api/conteo/hoy`).subscribe(res => {
             this.conteo = res;
             console.log(this.conteo);
-    });
-  }
-  async presentToast( message: string) {
-    const toast = await this.toastCtrl.create({
-      message,
-      duration: 2000
-    });
-    toast.present();
-  }
+            var i: number;
+            i = 0;
+            var j: number;
+            j = 0;
 
+            this.http.get(`http://localhost:5000/api/users/`).subscribe(res => {
+                this.usuarios = res;
+                console.log(this.usuarios);
 
-  favorite( conteo ) {
-    // console.log('favorite', user);
-    this.presentToast('Guardó en favoritos');
-    this.lista.closeSlidingItems();
-  }
+                for (i = 0; i < this.conteo.length; i++) {
+                    this.diezmo += parseFloat(this.conteo[i].diezmo);
+                    if (this.conteo[i].ofrenda) {
+                        this.ofrenda += parseFloat(this.conteo[i].ofrenda);
+                    }
+                    if (this.conteo[i].primicia) {
+                        this.primicia += parseFloat(this.conteo[i].primicia);
+                    }
+                    if (this.conteo[i].inversion) {
+                        this.inversion += parseFloat(this.conteo[i].inversion);
+                    }
+                    if (this.conteo[i].inversion) {
+                        this.inversion += parseFloat(this.conteo[i].inversion);
+                    }
+                    if (this.conteo[i].cumpleanos) {
+                        this.cumpleanos += parseFloat(
+                            this.conteo[i].cumpleanos
+                        );
+                    }
+                    if (this.conteo[i].agradecimientos) {
+                        this.agradecimientos += parseFloat(
+                            this.conteo[i].agradecimientos
+                        );
+                    }
+                    if (this.conteo[i].otros) {
+                        this.otros = parseFloat(this.conteo[i].otros);
+                    }
 
-
-  share( conteo ) {
-    this.presentToast('Información!');
-    this.lista.closeSlidingItems();
-    this.info_register(conteo);
-  }
-
-  async borrar( conteo ) {
-    
-    const alert = await this.alertCtrl.create({
-      header: "Confirmación",
-      message: "¿Deseas <stong>eliminar</strong> este registro?",
-      buttons: [
-          {
-              text: "Cancelar",
-              role: "cancel",
-              cssClass: "secondary",
-              handler: blah => {
-                  console.log("Confirm Cancel: Cancelar");
-              }
-          },
-          {
-              text: "Eliminar",
-              handler: () => {
-                  console.log("ENTRO A HANDLER");
-                  this.deleteConteo(conteo);
-                  this.presentToast('Borrado!');
-                  this.lista.closeSlidingItems();
-              }
-          }
-      ]
-    });
-    await alert.present();
-  }
-  deleteConteo(conteo) {
-    console.log("Función eliminar ...");
-    this.http.delete(this.url + conteo._id).subscribe(
-        val => {
-            console.log(
-                "DELETE call successful value returned in body",
-                val
-            );
-        },
-        response => {
-            console.log("DELETE call in error", response);
-        },
-        () => {
-            console.log("The DELETE observable is now completed.");
-            this.http.get(`http://localhost:5000/api/conteo/`).subscribe(res => {
-            this.conteo = res;
-            console.log(this.conteo);
-    });
-          }
-    );
-    //location.replace(document.referrer);
-    
-  }
-  toggleMenu() {
-    this.menuCtrl.toggle();
-  } 
-  info_register(conteo) {
-    //this.navCtrl.navigateForward(InfoMiembro, { id: usuario._id });
-    let navigationExtras: NavigationExtras = {
-        state: {
-            conteo: conteo
-        }
-    };
-    this.router.navigate(["info-register"], navigationExtras);
-  }
-
-  textoBuscar = "";
-  buscar( event ) {
-    // console.log(event);
-    this.textoBuscar = event.detail.value;
-  }
-
-   diezmo = 123.67;
-   ofrenda = 23.50;
-   primicia = 12.00;
-   inversion = 0.00;
-   cumpleaños = 0.00;
-   agradecimientos = 34.50;
-   otros = 0.00;
-   total = this.diezmo + this.ofrenda + this.primicia + this.inversion + this.cumpleaños + this.agradecimientos + this.otros;
-
-    
-    async presentActionSheet() {
-        const actionSheet = await this.actionSheetCtrl.create({
-          header: 'Subtotales',
-          backdropDismiss: false,
-          buttons: [{
-            text: 'Diezmo = $'+ this.diezmo,
-            role: 'destructive',
-            icon: 'logo-usd',
-            handler: () => {
-              console.log('Diezmo subtotal');
-            }
-          }, {
-            text: 'Ofrenda = $'+this.ofrenda,
-            icon: 'logo-usd',
-            handler: () => {
-              console.log('Share clicked');
-            }
-          }, {
-            text: 'Primicias = $'+this.primicia,
-            icon: 'logo-usd',
-            handler: () => {
-              console.log('Play clicked');
-            }
-          }, {
-            text: 'Inversion = $'+this.inversion,
-            icon: 'logo-usd',
-            handler: () => {
-              console.log('Favorite clicked');
-            }
-          },{
-            text: 'Cumpleaños = $'+this.cumpleaños,
-            icon: 'logo-usd',
-            handler: () => {
-              console.log('Play clicked');
-            }
-          },{
-            text: 'Agradecimientos = $'+this.agradecimientos,
-            icon: 'logo-usd',
-            handler: () => {
-              console.log('Play clicked');
-            }
-          },{
-            text: 'Otros = $'+this.otros,
-            icon: 'logo-usd',
-            handler: () => {
-              console.log('Play clicked');
-            }
-          },{
-            text: 'TOTAL = $'+this.total,
-            role: 'destructive',
-            icon: 'cash',
-            cssClass: 'rojo',
-            handler: () => {
-              console.log('Play clicked');
-            }
-          },{
-            text: 'Cerrar',
-            icon: 'close',
-            role: 'cancel',
-            handler: () => {
-              console.log('Cancel clicked');
-            }
-          }
-        ]
+                    console.log("ASDASDFASDFASDF");
+                    for (j = 0; j < this.usuarios.length; j++) {
+                        console.log("BBBBBBBBBBBBBBBBBBB");
+                        console.log("CONTEO USER ID" + this.conteo[i].user);
+                        console.log("USER ID " + this.usuarios[j]._id);
+                        if (
+                            this.conteo[i].user.toString() ==
+                            this.usuarios[j]._id.toString()
+                        ) {
+                            console.log("MATCH!");
+                            this.conteo[i].username =
+                                this.usuarios[j].nombre.toString() +
+                                " " +
+                                this.usuarios[j].apellidos.toString();
+                        }
+                    }
+                }
+                this.total =
+                    this.diezmo +
+                    this.ofrenda +
+                    this.primicia +
+                    this.inversion +
+                    this.cumpleanos +
+                    this.agradecimientos +
+                    this.otros;
+            });
         });
-    
-        await actionSheet.present();
+
+        //var usuario = "";
+    }
+    async presentToast(message: string) {
+        const toast = await this.toastCtrl.create({
+            message,
+            duration: 2000
+        });
+        toast.present();
     }
 
-    
-    
+    favorite(conteo) {
+        // console.log('favorite', user);
+        this.presentToast("Guardó en favoritos");
+        this.lista.closeSlidingItems();
+    }
 
+    /*share(conteo) {
+        this.presentToast("Información!");
+        this.lista.closeSlidingItems();
+        this.info_register(conteo);
+    }*/
 
+    async borrar(conteo) {
+        const alert = await this.alertCtrl.create({
+            header: "Confirmación",
+            message: "¿Deseas <stong>eliminar</strong> este registro?",
+            buttons: [
+                {
+                    text: "Cancelar",
+                    role: "cancel",
+                    cssClass: "secondary",
+                    handler: blah => {
+                        console.log("Confirm Cancel: Cancelar");
+                    }
+                },
+                {
+                    text: "Eliminar",
+                    handler: () => {
+                        console.log("ENTRO A HANDLER");
+                        this.deleteConteo(conteo);
+                        this.presentToast("Borrado!");
+                        this.lista.closeSlidingItems();
+                    }
+                }
+            ]
+        });
+        await alert.present();
+    }
+    deleteConteo(conteo) {
+        console.log("Función eliminar ...");
+        this.http.delete(this.url + conteo._id).subscribe(
+            val => {
+                console.log(
+                    "DELETE call successful value returned in body",
+                    val
+                );
+            },
+            response => {
+                console.log("DELETE call in error", response);
+            },
+            () => {
+                console.log("The DELETE observable is now completed.");
+                this.http
+                    .get(`http://localhost:5000/api/conteo/hoy`)
+                    .subscribe(res => {
+                        this.conteo = res;
+                        console.log(this.conteo);
+                    });
+            }
+        );
+        //location.replace(document.referrer);
+    }
+    toggleMenu() {
+        this.menuCtrl.toggle();
+    }
+    info_register(conteo) {
+        //this.navCtrl.navigateForward(InfoMiembro, { id: usuario._id });
+
+        let navigationExtras: NavigationExtras = {
+            state: {
+                conteo: conteo
+            }
+        };
+        this.router.navigate(["info-register"], navigationExtras);
+    }
+
+    textoBuscar = "";
+    buscar(event) {
+        // console.log(event);
+        this.textoBuscar = event.detail.value;
+    }
+
+    diezmo = 0.0;
+    ofrenda = 0.0;
+    primicia = 0.0;
+    inversion = 0.0;
+    cumpleanos = 0.0;
+    agradecimientos = 0.0;
+    otros = 0.0;
+    total = 0.0;
+
+    async presentActionSheet() {
+        const actionSheet = await this.actionSheetCtrl.create({
+            header: "Subtotales",
+            backdropDismiss: false,
+            buttons: [
+                {
+                    text: "Diezmo = $" + this.diezmo,
+                    role: "destructive",
+                    icon: "logo-usd",
+                    handler: () => {
+                        console.log("Diezmo subtotal");
+                    }
+                },
+                {
+                    text: "Ofrenda = $" + this.ofrenda,
+                    icon: "logo-usd",
+                    handler: () => {
+                        console.log("Share clicked");
+                    }
+                },
+                {
+                    text: "Primicias = $" + this.primicia,
+                    icon: "logo-usd",
+                    handler: () => {
+                        console.log("Play clicked");
+                    }
+                },
+                {
+                    text: "Inversion = $" + this.inversion,
+                    icon: "logo-usd",
+                    handler: () => {
+                        console.log("Favorite clicked");
+                    }
+                },
+                {
+                    text: "Cumpleaños = $" + this.cumpleanos,
+                    icon: "logo-usd",
+                    handler: () => {
+                        console.log("Play clicked");
+                    }
+                },
+                {
+                    text: "Agradecimientos = $" + this.agradecimientos,
+                    icon: "logo-usd",
+                    handler: () => {
+                        console.log("Play clicked");
+                    }
+                },
+                {
+                    text: "Otros = $" + this.otros,
+                    icon: "logo-usd",
+                    handler: () => {
+                        console.log("Play clicked");
+                    }
+                },
+                {
+                    text: "TOTAL = $" + this.total,
+                    role: "destructive",
+                    icon: "cash",
+                    cssClass: "rojo",
+                    handler: () => {
+                        console.log("Play clicked");
+                    }
+                },
+                {
+                    text: "Cerrar",
+                    icon: "close",
+                    role: "cancel",
+                    handler: () => {
+                        console.log("Cancel clicked");
+                    }
+                }
+            ]
+        });
+
+        await actionSheet.present();
+    }
 }
